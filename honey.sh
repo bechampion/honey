a9b4375c554733b05abaaf68b45fe2833d084ff5
jeronimo <garciaj.uk@gmail.com>
2017-12-08 09:27:51 +0000

    Rolling back to auditd as strace wont work

--- honey.sh
+++ honey.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-DIP=$(docker inspect $(docker run --privileged=false --cap-drop all --cap-add SYS_CHROOT --cap-add SETGID --cap-add SETUID --cap-add CHOWN  -dt honey) --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
+DID=$(docker run --privileged=false --cap-drop all --cap-add SYS_CHROOT --cap-add SETGID --cap-add SETUID --cap-add CHOWN  -dt honey)
+DIP=$(docker inspect ${DID} --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
 exec /usr/bin/socat stdin tcp:${DIP}:2222,retry=60
