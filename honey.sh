0c7225e63471f6279464354cd20d6a1ae0343498
bechampion <garciaj.uk@gmail.com>
2017-12-11 11:07:15 +0000

    changing image

--- honey.sh
+++ honey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DID=$(docker run --read-only --privileged=false --cap-drop all --cap-add SYS_CHROOT --cap-add SETGID --cap-add SETUID --cap-add CHOWN  -dt honey)
+DID=$(docker run --read-only --privileged=false --cap-drop all --cap-add SYS_CHROOT --cap-add SETGID --cap-add SETUID --cap-add CHOWN  -dt bechampion/honey)
 DPID=$(docker inspect ${DID} --format='{{ .State.Pid}}')
 DIP=$(docker inspect ${DID} --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
 echo "${DID} ${DIP}" :: $(date) >> /tmp/track
